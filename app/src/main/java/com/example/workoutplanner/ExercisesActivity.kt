52895adc547eb059c8f82e95c5d5ccd6cc57fa52
ondrej.koula <ondrej.koula@memsource.com>
2019-06-11 18:18:23 +0200

    UI - exercises create form

--- app/src/main/java/com/example/workoutplanner/ExercisesActivity.kt
+++ app/src/main/java/com/example/workoutplanner/ExercisesActivity.kt
@@ -1,12 +1,22 @@
 package com.example.workoutplanner
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.workoutplanner.exercise.SelectTypeActivity
+import kotlinx.android.synthetic.main.activity_exercises.*
 
 class ExercisesActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercises)
+
+        btnCreateExercise.setOnClickListener {
+            var selectTypeIntent = Intent(this, SelectTypeActivity::class.java)
+            startActivity(selectTypeIntent)
+        }
+
     }
 }
