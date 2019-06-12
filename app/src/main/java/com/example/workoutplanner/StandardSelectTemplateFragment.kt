4cf60948555442fb7b24cea909b852e80feeb648
ondrej.koula <ondrej.koula@memsource.com>
2019-06-13 00:32:10 +0200

    UI - dropdown

--- app/src/main/java/com/example/workoutplanner/StandardSelectTemplateFragment.kt
+++ app/src/main/java/com/example/workoutplanner/StandardSelectTemplateFragment.kt
@@ -5,29 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 
-class StandardSelectTemplateFragment : Fragment() {
+class StandardSelectTemplateFragment : Fragment(), AdapterView.OnItemSelectedListener {
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_standard_select_template, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
         view?.let {
             val templateSpinner: Spinner = it.findViewById(R.id.templateSpinner)
+            templateSpinner.onItemSelectedListener = this
             ArrayAdapter.createFromResource(
-                this.context,
+                this.context!!,
                 R.array.exerciseTemplates,
-                android.R.layout.simple_spinner_item
+                R.layout.spinner_color_layout
 
             ).also { adapter ->
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                adapter.setDropDownViewResource(R.layout.spiner_dropdown_layout)
                 templateSpinner.adapter = adapter
             }
         }
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
